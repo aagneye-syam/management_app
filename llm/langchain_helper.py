1f321733d088959a5b9203a9dc07a4b72776633b
aagneye-syam <saagneye2003@gmail.com>
2024-07-04 20:06:32 +0530

    llm init

--- llm/langchain_helper.py
+++ llm/langchain_helper.py
@@ -1,1 +1,10 @@
 from langchain.llms import GooglePalm
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = GooglePalm(google_api_key=["API_KEY"], temperature = 0.9)
+
+paragraph =  llm(input(print("enter some that i can talk to you")))
+
+print(paragraph)
